80a068c97d342b8481cd44a2334188293038cc8e
jajla <kamilmichalski204@gmail.com>
2025-01-30 15:28:15 +0100

    Rozbudowa modelu User i Migracji dla niego o nowe pola

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'phone_numer',
+        'role',
         'email',
         'password',
     ];
