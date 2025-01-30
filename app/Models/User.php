a6944767496579d1cfeb1d1d29af18f43f8558b7
jajla <kamilmichalski204@gmail.com>
2025-01-30 21:32:41 +0100

    dodanie nowych pol dla formularza do rejstracji

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
-        'phone_numer',
+        'phone_number',
         'role',
         'email',
         'password',
